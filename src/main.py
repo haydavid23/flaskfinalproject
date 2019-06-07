f68b78fc956f9f9ca0a4e24f665f3b53bd060ffb
haydavid23 <hay.david@hotmail.com>
2019-06-07 17:51:23 +0000

    working

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
 from utils import APIException, generate_sitemap
 from models import db, Person
 
@@ -15,6 +18,42 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
+
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    username = params.get('username', None)
+    password = params.get('password', None)
+
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    usercheck = Person.query.filter_by(username=username, password=password).first()
+    if usercheck == None:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    #if username != 'test' or password != 'test':
+    #    return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=username)}
+    return jsonify(ret), 200
+
+# Protect a view with jwt_required, which requires a valid jwt
+# to be present in the headers.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    return jsonify({'hello_from': get_jwt_identity()}), 200
 
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -41,7 +80,7 @@ def handle_person():
         if 'email' not in body:
             raise APIException('You need to specify the email', status_code=400)
 
-        user1 = Person(username=body['username'], email=body['email'])
+        user1 = Person(username=body['username'], email=body['email'], name=body['name'], password=body['password'], last_name=['last_name'])
         db.session.add(user1)
         db.session.commit()
         return "ok", 200
--- src/models.py
+++ src/models.py
@@ -4,7 +4,10 @@ db = SQLAlchemy()
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    last_name = db.Column(db.String(80), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.Integer, unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
@@ -13,5 +16,8 @@ class Person(db.Model):
     def serialize(self):
         return {
             "username": self.username,
-            "email": self.email
+            "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name,
+            "password": self.password
         }
